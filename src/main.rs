00d2f070b3b09bdfb9774d0080e2be41aee5fa72
RMcTn <rmctiernan@protonmail.com>
2023-07-28 15:51:37 +0100

    Pull out address form work into method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::{Datelike, NaiveDate};
 use fantoccini::elements::Element;
-use fantoccini::{ClientBuilder, Locator};
+use fantoccini::{Client, ClientBuilder, Locator};
 use std::error::Error;
 use std::time::Duration;
 use std::{dbg, eprintln, format};
@@ -9,83 +9,11 @@ use std::{dbg, eprintln, format};
 async fn main() -> Result<(), Box<dyn Error>> {
     // NOTE: Some of the fields get different IDs when submitting each step it seems
 
-    let bins_url = "https://www.northlanarkshire.gov.uk/bin-collection-dates";
-
-    let postcode_input_id = "address-finder-postcode-search-text";
-    let postcode = "***REMOVED***";
-
-    let find_address_input_id = "address_finder-postcode-search-button";
-
-    let confirm_button_id = "address_finder_confirm_address_selection";
-
-    let next_button_name = "op";
-
     let client = ClientBuilder::native()
         .connect("http://localhost:4444")
         .await?;
-    client.goto(bins_url).await?;
 
-    dbg!("Waiting for cookie confirmation");
-    client
-        .find(Locator::Css(".cb-enable"))
-        .await?
-        .click()
-        .await?;
-    dbg!("Clicked cookie");
-
-    dbg!("Little sleep for page load");
-    tokio::time::sleep(Duration::from_secs(1)).await;
-    dbg!("Waiting for postcode input box");
-    let postcode_input = client.find(Locator::Id(postcode_input_id)).await?;
-
-    postcode_input.click().await?;
-    dbg!("Clicked postcode input box");
-    // Enter key doesn't submit this form
-    postcode_input.send_keys(postcode).await?;
-    // TODO - Check the input box to make sure a value is selected (or rely on the confirm part
-    // after?)
-
-    dbg!("Waiting for find address button");
-    client
-        .find(Locator::Id(find_address_input_id))
-        .await?
-        .click()
-        .await?;
-    dbg!("Submitted find address button");
-    dbg!("Little sleep for page load");
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    dbg!("Waiting for address drop down");
-    let address_drop_down = client.find(Locator::Css("select.form-select")).await?;
-    address_drop_down.click().await?;
-    dbg!("Clicked address drop down");
-
-    address_drop_down.send_keys("***REMOVED***").await?;
-    // TODO: Is Enter needed here?
-    dbg!("Waiting for confirm address button");
-    client
-        .find(Locator::Id(confirm_button_id))
-        .await?
-        .click()
-        .await?;
-    dbg!("Clicked confirm address button");
-    // Ignoring successful address lookup check
-
-    dbg!("Waiting for next button");
-    client
-        .find(Locator::Css(&format!("input[name={}]", next_button_name)))
-        .await?
-        .click()
-        .await?;
-    dbg!("Clicked next button");
-
-    dbg!("Waiting for next page");
-    client
-        .wait()
-        .for_element(Locator::Css(".bin-collection-dates-container"))
-        .await?;
-
-    dbg!("On next page");
+    fill_out_address_form(&client).await?;
 
     let black_bins_div = client
         .find(Locator::Css(".waste-type--general-waste"))
@@ -166,6 +94,84 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     dbg!(next_bin_collection);
 
+    return Ok(());
+}
+
+async fn fill_out_address_form(client: &Client) -> Result<(), Box<dyn Error>> {
+    let bins_url = "https://www.northlanarkshire.gov.uk/bin-collection-dates";
+
+    let postcode_input_id = "address-finder-postcode-search-text";
+    let postcode = "***REMOVED***";
+
+    let find_address_input_id = "address_finder-postcode-search-button";
+
+    let confirm_button_id = "address_finder_confirm_address_selection";
+
+    let next_button_name = "op";
+
+    client.goto(bins_url).await?;
+
+    dbg!("Waiting for cookie confirmation");
+    client
+        .find(Locator::Css(".cb-enable"))
+        .await?
+        .click()
+        .await?;
+    dbg!("Clicked cookie");
+
+    dbg!("Little sleep for page load");
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    dbg!("Waiting for postcode input box");
+    let postcode_input = client.find(Locator::Id(postcode_input_id)).await?;
+
+    postcode_input.click().await?;
+    dbg!("Clicked postcode input box");
+    // Enter key doesn't submit this form
+    postcode_input.send_keys(postcode).await?;
+    // TODO - Check the input box to make sure a value is selected (or rely on the confirm part
+    // after?)
+
+    dbg!("Waiting for find address button");
+    client
+        .find(Locator::Id(find_address_input_id))
+        .await?
+        .click()
+        .await?;
+    dbg!("Submitted find address button");
+    dbg!("Little sleep for page load");
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    dbg!("Waiting for address drop down");
+    let address_drop_down = client.find(Locator::Css("select.form-select")).await?;
+    address_drop_down.click().await?;
+    dbg!("Clicked address drop down");
+
+    address_drop_down.send_keys("***REMOVED***").await?;
+    // TODO: Is Enter needed here?
+    dbg!("Waiting for confirm address button");
+    client
+        .find(Locator::Id(confirm_button_id))
+        .await?
+        .click()
+        .await?;
+    dbg!("Clicked confirm address button");
+    // Ignoring successful address lookup check
+
+    dbg!("Waiting for next button");
+    client
+        .find(Locator::Css(&format!("input[name={}]", next_button_name)))
+        .await?
+        .click()
+        .await?;
+    dbg!("Clicked next button");
+
+    dbg!("Waiting for next page");
+    client
+        .wait()
+        .for_element(Locator::Css(".bin-collection-dates-container"))
+        .await?;
+
+    dbg!("On next page");
     return Ok(());
 }
 
