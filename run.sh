570abcc7f64dd56239776c9ac00f4d0a53c67cb1
RMcTn <rmctiernan@protonmail.com>
2023-07-30 01:37:14 +0100

    Fix Firefox hanging on Ubunutu 22.04 with snap

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 pkill geckodriver
-geckodriver &
+# See https://firefox-source-docs.mozilla.org/testing/geckodriver/Usage.html#Running-Firefox-in-an-container-based-package 
+# for why we need to define a profile root
+mkdir $HOME/geckodriver-profiles
+geckodriver --profile-root $HOME/geckodriver-profiles &
+
 cargo run
