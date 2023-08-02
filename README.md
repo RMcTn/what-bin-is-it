cbfdc9d76e7d3c309a99919b15a2d14245f51d84
RMcTn <rmctiernan@protonmail.com>
2023-08-02 19:49:09 +0100

    Load people to notify from DB

--- README.md
+++ README.md
@@ -8,10 +8,8 @@ Run ```geckodriver``` before running the program
 ## Required ENV vars
 AWS_ACCESS_KEY_ID  
 AWS_SECRET_ACCESS_KEY  
-TO_EMAIL_ADDRESS  
 FROM_EMAIL_ADDRESS  
-POSTCODE  
-HOME_ADDRESS  
+DB_FILENAME
 
 ## Cross compile
 ```cargo install cross --git https://github.com/cross-rs/cross```
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use chrono::{Datelike, NaiveDate};
 use std::error::Error;
 use std::{dbg, env};
 
+use rusqlite::Result;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
 
-    let to_email_address =
-        env::var("TO_EMAIL_ADDRESS").expect("TO_EMAIL_ADDRESS must be specified");
     let from_email_address =
         env::var("FROM_EMAIL_ADDRESS").expect("FROM_EMAIL_ADDRESS must be specified");
 
@@ -22,89 +22,117 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let _aws_secret_access_key =
         env::var("AWS_SECRET_ACCESS_KEY").expect("AWS_SECRET_ACCESS_KEY must be specified");
 
-    let postcode = env::var("POSTCODE").expect("POSTCODE must be specified");
-    let home_address = env::var("HOME_ADDRESS").expect("HOME_ADDRESS must be specified");
+    let db_filename = env::var("DB_FILENAME").expect("DB_FILENAME must be specified");
 
     let region_provider = RegionProviderChain::default_provider().or_else("eu-west-1");
     let config = aws_config::from_env().region(region_provider).load().await;
     let aws_client = Client::new(&config);
 
+    let db_conn = rusqlite::Connection::open(db_filename)?;
+    db_conn.execute(
+        "CREATE TABLE IF NOT EXISTS emails (
+            id          INTEGER PRIMARY KEY,
+            email       TEXT NOT NULL,
+            postcode    TEXT NOT NULL,
+            address     TEXT NOT NULL
+        )",
+        (),
+    )?;
+
+    db_conn.execute(
+        "CREATE UNIQUE INDEX IF NOT EXISTS EmailsUniqueIndexOnEmails ON emails (email)",
+        (),
+    )?;
+
+    let mut stmt = db_conn.prepare("SELECT id, email, postcode, address FROM emails")?;
+    let people_to_notify = stmt.query_map([], |row| {
+        Ok(ToBeNotified {
+            _id: row.get(0)?,
+            email: row.get(1)?,
+            postcode: row.get(2)?,
+            address: row.get(3)?,
+        })
+    })?;
+
     // Need to scrape the page from an actual browser. Tried curl/reqwest requests, but submitting
     // the post request would redirect back to the first page. Some sort of request token missing
     // when we do this or something (first step of form submission changes the url).
-    let bins = scraper::get_stuff(&postcode, &home_address).await?;
+    for person in people_to_notify {
+        let person = person.unwrap();
+        println!("Found {:?}", person);
+        let bins = scraper::get_stuff(&person.postcode, &person.address).await?;
 
-    let today = chrono::Utc::now().date_naive();
-    let next_collection_date = next_collection_date_from(today);
+        let today = chrono::Utc::now().date_naive();
+        let next_collection_date = next_collection_date_from(today);
 
-    dbg!(next_collection_date);
-    let mut next_collection_day_for_bins = Vec::new();
-    for bin in bins {
-        let next_day = next_collection_date_for_bin(&bin, next_collection_date);
-        next_collection_day_for_bins.push(next_day);
-    }
-
-    let mut closest_bin_day = next_collection_day_for_bins[0];
-    let mut closest_bin_days = Vec::new();
-    for bin_day in next_collection_day_for_bins {
-        if bin_day.date == closest_bin_day.date {
-            closest_bin_days.push(bin_day);
+        dbg!(next_collection_date);
+        let mut next_collection_day_for_bins = Vec::new();
+        for bin in bins {
+            let next_day = next_collection_date_for_bin(&bin, next_collection_date);
+            next_collection_day_for_bins.push(next_day);
         }
-        if bin_day.date < closest_bin_day.date {
-            closest_bin_day = bin_day;
-            closest_bin_days.clear();
-            closest_bin_days.push(bin_day);
+
+        let mut closest_bin_day = next_collection_day_for_bins[0];
+        let mut closest_bin_days = Vec::new();
+        for bin_day in next_collection_day_for_bins {
+            if bin_day.date == closest_bin_day.date {
+                closest_bin_days.push(bin_day);
+            }
+            if bin_day.date < closest_bin_day.date {
+                closest_bin_day = bin_day;
+                closest_bin_days.clear();
+                closest_bin_days.push(bin_day);
+            }
         }
+
+        let next_bin_collection = NextBinCollection {
+            bins: closest_bin_days,
+        };
+
+        println!("Next bins:");
+        let mut bin_email_body = String::new();
+
+        let subject = bins_subject(&next_bin_collection);
+
+        for bin_day in next_bin_collection.bins {
+            let s = format!(
+                "{} bin is being collected on {}\n",
+                bin_day.bin, bin_day.date
+            );
+            bin_email_body.push_str(&s);
+        }
+
+        let destination_email = Destination::builder().to_addresses(&person.email).build();
+
+        let subject_content = Content::builder().data(subject).charset("UTF-8").build();
+
+        let body_content = Content::builder()
+            .data(bin_email_body)
+            .charset("UTF-8")
+            .build();
+
+        let body = Body::builder().text(body_content).build();
+
+        let msg = Message::builder()
+            .subject(subject_content)
+            .body(body)
+            .build();
+
+        dbg!(&msg);
+
+        let email_content = EmailContent::builder().simple(msg).build();
+
+        println!("About to send email to {}", &person.email);
+        aws_client
+            .send_email()
+            .from_email_address(&from_email_address)
+            .destination(destination_email)
+            .content(email_content)
+            .send()
+            .await?;
+        println!("Email sent");
     }
 
-    let next_bin_collection = NextBinCollection {
-        bins: closest_bin_days,
-    };
-
-    println!("Next bins:");
-    let mut bin_email_body = String::new();
-
-    let subject = bins_subject(&next_bin_collection);
-
-    for bin_day in next_bin_collection.bins {
-        let s = format!(
-            "{} bin is being collected on {}\n",
-            bin_day.bin, bin_day.date
-        );
-        bin_email_body.push_str(&s);
-    }
-
-    let destination_email = Destination::builder()
-        .to_addresses(to_email_address)
-        .build();
-
-    let subject_content = Content::builder().data(subject).charset("UTF-8").build();
-
-    let body_content = Content::builder()
-        .data(bin_email_body)
-        .charset("UTF-8")
-        .build();
-
-    let body = Body::builder().text(body_content).build();
-
-    let msg = Message::builder()
-        .subject(subject_content)
-        .body(body)
-        .build();
-
-    dbg!(&msg);
-
-    let email_content = EmailContent::builder().simple(msg).build();
-
-    println!("About to send email");
-    aws_client
-        .send_email()
-        .from_email_address(from_email_address)
-        .destination(destination_email)
-        .content(email_content)
-        .send()
-        .await?;
-    println!("Email sent");
     return Ok(());
 }
 
@@ -286,4 +314,13 @@ mod tests {
         let subject = bins_subject(&next_bin_collection);
         assert_eq!(subject, "Blue bin out tonight");
     }
+}
+
+#[derive(Debug)]
+struct ToBeNotified {
+    _id: i32,
+    // TODO: Better types for these with some validation?
+    email: String,
+    postcode: String,
+    address: String,
 }
