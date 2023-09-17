232164fa30cce1837b1cb4c18314217f3e44ce8f
RMcTn <rmctiernan@protonmail.com>
2023-09-17 21:05:09 +0100

    Better deployment script

--- deploy_binary_to_host.sh
+++ deploy_binary_to_host.sh
@@ -4,5 +4,9 @@
 if [ -z $1 ]; then 
 	echo "Need to provide a host";
 else
-	scp -r ./target/x86_64-unknown-linux-gnu/release/server root@$1:/root/what-bin-is-it/
+	ssh root@$1 "mkdir -p /root/what-bin-is-it/archive"
+	scp -r ./target/x86_64-unknown-linux-gnu/release/server root@$1:/root/what-bin-is-it/server-new
+	ssh root@$1 "mv /root/what-bin-is-it/server /root/what-bin-is-it/archive/server_$(date +"%Y%m%d%H%M%S")"
+	ssh root@$1 "mv /root/what-bin-is-it/server-new /root/what-bin-is-it/server"
+	ssh root@$1 "systemctl restart whatbin"
 fi
